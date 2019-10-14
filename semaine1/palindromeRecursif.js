1ac17ad3f82c54eaa3a6477cbd27bbcb23716a12
TimFrHunter <samsontim@hotmail.fr>
2019-10-14 16:30:01 +0200

    add recursif palindrome

---
+++ semaine1/palindromeRecursif.js
@@ -0,0 +1,19 @@
+function palindrome(mot){
+   
+    if(mot.length == 0  || mot.length == 1)
+        return true
+    else if(mot[0] == mot[mot.length - 1]){
+        mot = mot.substr(1, mot.length-2)//mot.length -2 parceque le substr coupe le mot une fois du coup c'est l'ancinne longueur est il faut faire -2
+        return palindrome(mot)
+    } else
+        return false  
+  
+}
+
+function estPalindrome(chaine){
+    console.log("La chaine", chaine, " : ", palindrome(chaine))
+}
+
+estPalindrome("A")
+estPalindrome("BoB")
+estPalindrome("ANOYNA")
